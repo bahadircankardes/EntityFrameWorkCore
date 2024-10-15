728df111f510d4e59d19c63c6b984b424e8bb1a9
bahadircankardes <kardes126@gmail.com>
2024-10-15 20:42:00 +0300

    asdres ve personel entityleri

---
+++ 3-CodeFirst/Models/Personel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace _3_CodeFirst.Models
+{
+	public class Personel
+	{
+		public Personel()
+		{
+		}
+	}
+}
+
--- 3-CodeFirst/obj/Debug/net7.0/3-CodeFirst.AssemblyInfo.cs
+++ 3-CodeFirst/obj/Debug/net7.0/3-CodeFirst.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3-CodeFirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd57133cacc3087d253b20b4081aaa14410b54e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("3-CodeFirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3-CodeFirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
